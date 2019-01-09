536d69dce10b0e008c658d704c4905892aae76a0
PB2 <ltbergeron@gmail.com>
2019-01-09 00:57:42 -0500

    Update ExampleLatLon.ino

--- examples/ExampleLatLon/ExampleLatLon.ino
+++ examples/ExampleLatLon/ExampleLatLon.ino
@@ -3,7 +3,7 @@
 
 void setup()
 {
-  Serial.begin(2000000);
+  Serial.begin(115200);
   while (!Serial);
 
   Serial3.begin(9600);
@@ -18,6 +18,13 @@ void setup()
 void loop()
 {
   myGPS.grabData_LatLong();
+  
+  //debugging prints
+	Serial.print("UTC HOUR: "); Serial.println(myGPS.GPS_data[UTC_HOUR_POS]);
+	Serial.print("UTC MIN: "); Serial.println(myGPS.GPS_data[UTC_MINUTE_POS]);
+	Serial.print("UTC SEC: "); Serial.println(myGPS.GPS_data[UTC_SECOND_POS]);
+	Serial.print("LAT (dd): "); Serial.println(myGPS.GPS_data[LAT_POS], 10);
+	Serial.print("LON (dd): "); Serial.println(myGPS.GPS_data[LON_POS], 10);
 }
 
 
