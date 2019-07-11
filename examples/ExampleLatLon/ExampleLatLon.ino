ace2db1b2651893c6cbab1db64d3914f506c6c44
PB2 <ltbergeron@gmail.com>
2019-07-11 17:59:17 -0400

    bug fix

--- examples/ExampleLatLon/ExampleLatLon.ino
+++ examples/ExampleLatLon/ExampleLatLon.ino
@@ -30,8 +30,8 @@ void loop()
     Serial.print(myGPS.navStatus); Serial.print(" | ");
     Serial.print(myGPS.lat);       Serial.print(" | ");
     Serial.print(myGPS.latDir);    Serial.print(" | ");
-    Serial.print(myGPS.lat);       Serial.print(" | ");
-    Serial.print(myGPS.latDir);    Serial.print(" | ");
+    Serial.print(myGPS.lon);       Serial.print(" | ");
+    Serial.print(myGPS.lonDir);    Serial.print(" | ");
     Serial.print(myGPS.sog_knots); Serial.print(" | ");
     Serial.print(myGPS.cog_true);  Serial.print(" | ");
     Serial.println();
