3edc677feee77eed3b6b2cc54de4f2e5f592407e
PB2 <ltbergeron@gmail.com>
2019-07-10 21:36:33 -0400

    Update neo6mGPS.h

--- neo6mGPS.h
+++ neo6mGPS.h
@@ -10,28 +10,34 @@
 
 
 
-const byte NMEA_LEN       = 16;
-const byte FREQ_LEN       = 14;
-const byte BAUD_LEN       = 28;
+const byte NMEA_LEN = 16;
+const byte FREQ_LEN = 14;
+const byte BAUD_LEN = 28;
 
-const byte GPS_NO_DATA    = 0;
-const byte GPS_NEW_DATA   = 1;
+const byte GPS_NO_DATA  = 0;
+const byte GPS_NEW_DATA = 1;
 
-const byte GPGGA          = 0;
-const byte GPGLL          = 1;
-const byte GPGLV          = 2;
-const byte GPGSA          = 3;
-const byte GPRMC          = 4;
-const byte GPVTG          = 5;
+const byte GPGGA = 0;
+const byte GPGLL = 1;
+const byte GPGLV = 2;
+const byte GPGSA = 3;
+const byte GPRMC = 4;
+const byte GPVTG = 5;
 
-const byte NMEA_ID_POS    = 7;
-const byte DDC_POS        = 13;
-const byte SERIAL_1_POS   = 14;
-const byte SERIAL_2_POS   = 15;
-const byte USB_POS        = 16;
-const byte SPI_POS        = 17;
-const byte CK_A           = 19;
-const byte CK_B           = 20;
+const byte NMEA_ID_POS  = 7;
+const byte DDC_POS      = 8;
+const byte SERIAL_1_POS = 9;
+const byte SERIAL_2_POS = 10;
+const byte USB_POS      = 11;
+const byte SPI_POS      = 12;
+
+const byte BAUD_0 = 14;
+const byte BAUD_1 = 15;
+const byte BAUD_2 = 16;
+const byte BAUD_3 = 17;
+
+const byte NAV_RATE_1 = 8;
+const byte NAV_RATE_2 = 9;
 
 
 
@@ -62,14 +68,14 @@ const char CFG_RATE[FREQ_LEN] = {
 	0x08, // id
 	0x06, // length
 	0x00, // length
-	0x64, // payload
-	0x00, // payload
-	0x01, // payload
-	0x00, // payload
-	0x01, // payload
-	0x00, // payload
-	0x7A, // CK_A
-	0x12  // CK_B
+	0x64, // payload measRate (ms) 1
+	0x00, // payload measRate (ms) 2
+	0x00, // payload navRate (cycles) 1
+	0x00, // payload navRate (cycles) 2
+	0x01, // payload timeRef 1
+	0x00, // payload timeRef 2
+	0x00, // CK_A
+	0x00  // CK_B
 };
 
 const char CFG_PRT[BAUD_LEN] = {
@@ -79,28 +85,28 @@ const char CFG_PRT[BAUD_LEN] = {
 	0x00, // id
 	0x14, // length
 	0x00, // length
-	0x01, // payload
-	0x00, // payload
-	0x00, // payload
-	0x00, // payload
-	0xD0, // payload
-	0x08, // payload
-	0x00, // payload
-	0x00, // payload
-	0x00, // payload
-	0xC2, // payload
-	0x01, // payload
-	0x00, // payload
-	0x07, // payload
-	0x00, // payload
-	0x03, // payload
-	0x00, // payload
-	0x00, // payload
-	0x00, // payload
-	0x00, // payload
-	0x00, // payload
-	0xC0, // CK_A
-	0x7E  // CK_B
+	0x01, // payload portID
+	0x00, // payload reserved0
+	0x00, // payload txReady 1
+	0x00, // payload txReady 2
+	0xD0, // payload mode 1
+	0x08, // payload mode 2
+	0x00, // payload mode 3
+	0x00, // payload mode 4
+	0x00, // payload baudRate 0 (LSB)
+	0x00, // payload baudRate 1
+	0x00, // payload baudRate 2
+	0x00, // payload baudRate 3 (MSB)
+	0x07, // payload inProtoMask 1
+	0x00, // payload inProtoMask 2
+	0x03, // payload outProtoMask 1
+	0x00, // payload outProtoMask 2
+	0x00, // payload reserved4 1
+	0x00, // payload reserved4 2
+	0x00, // payload reserved5 1
+	0x00, // payload reserved5 2
+	0x00, // CK_A
+	0x00  // CK_B
 };
 
 
@@ -109,26 +115,14 @@ const char CFG_PRT[BAUD_LEN] = {
 class neo6mGPS
 {
 public:// <<---------------------------------------------------------------------------//public
-	struct neo6m_data {
-		uint16_t utc_year;
-		uint8_t utc_month;
-		uint8_t utc_day;
-		uint8_t utc_hour;
-		uint8_t utc_min;
-		uint8_t utc_sec;
-		float lat_dd;
-		float lon_dd;
-		float sog; //Speed over ground (knots)
-		float cog; //Course over ground (degrees)
-	};
-	neo6m_data telem;
-
-
-
-
 	void begin(HardwareSerial &port);
 	void begin(usb_serial_class &port);
-	int grabData_GPS();
+	void setupGPS();
+	void disableAllNmea();
+	void enableAllNmea();
+	void setSentence(char NMEA_num, bool enable);
+	void changeBaud(uint32_t baud);
+	void changeFreq(uint16_t hertz);
 
 
 
@@ -138,34 +132,11 @@ private:// <<-------------------------------------------------------------------
 	usb_serial_class* usb_port;
 	bool usingUSB = false;
 
-	struct temp_neo6m_data {
-		char utc_year[NEO_BUFF_LEN];
-		char utc_month[NEO_BUFF_LEN];
-		char utc_day[NEO_BUFF_LEN];
-		char utc_hour[NEO_BUFF_LEN];
-		char utc_min[NEO_BUFF_LEN];
-		char utc_sec[NEO_BUFF_LEN];
-		char lat_dd[NEO_BUFF_LEN];
-		char lon_dd[NEO_BUFF_LEN];
-		char sog[NEO_BUFF_LEN]; //Speed over ground (knots)
-		char cog[NEO_BUFF_LEN]; //Course over ground (degrees)
-	};
-	temp_neo6m_data tempData;
-	
-	const byte header[NEO_HEADER_LEN] = { '$', 'G', 'P', 'R', 'M', 'C' };
-	byte headerIndex = 0;
-	byte fieldIndex = 0;
 
 
 
-
-	void setupGPS();
-	void setSentence(char NMEA_num, bool enable);
-	void disableAllNmea();
 	void enableSelectedNmea();
 	void insertChecksum(char packet[], const byte len);
-	void changeBaudrate();
-	void changeFrequency();
 	void sendPacket(char packet[], const byte len);
 	void sendPacket(const char packet[], const byte len);
 };
